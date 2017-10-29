01cec7a70788f5a29de6f6b4a2cee6f656d157ef
gitremote <12888046+gitremote@users.noreply.github.com>
2017-10-29 16:24:29 +0000

    Update test_utilities.py

--- test_utilities.py
+++ test_utilities.py
@@ -30,7 +30,29 @@ class TestAppend(unittest.TestCase):
         input_list = [1, 2, 3]
         output_list = utilities.append(4, input_list)
         self.assertEqual(output_list, [1, 2, 3, 4])
-        
 
+        
+class TestAddThreeNumbers(unittest.TestCase):
+
+    def test_add_simple(self):
+        output = utilities.add_three_numbers(1, 1, 1)
+        self.assertEqual(output, 3)
+
+    def test_add_complex(self):
+        output = utilities.add_three_numbers(1, 2, 3)
+        self.assertEqual(output, 6)
+
+
+class TestMultiplyThreeNumbers(unittest.TestCase):
+
+    def test_multiply_simple(self):
+        output = utilities.multiply_three_numbers(1, 1, 1)
+        self.assertEqual(1, output)
+
+    def test_multiply_complex(self):
+        output = utilities.multiply_three_numbers(5, 2, 3)
+        self.assertEqual(30, output)
+
+        
 if __name__ == '__main__':
     unittest.main()
